0ac2387a61b0c1910678f3b71c77aa936dd7f072
frno0254 <smallgreycreatures@gmail.com>
2014-12-16 21:40:25 +0100

    cleaned up version

--- macro.clj
+++ macro.clj
@@ -1,7 +1,14 @@
 (defmacro safe [references expression]
-	`(if (instance? java.io.Closeable ~(get references 1))
-	(let [~(get references 0) ~(get references 1)]
-	(try
-		~expression
-	(finally 
-		(if (instance? java.io.Closeable ~(get references 0)) (.close ~(get references 0))))))))
+	(if (vector? references)
+		`(let [~(get references 0) ~(get references 1)]
+			(try
+				~expression
+				(finally 
+					(if (instance? java.io.Closeable ~(get references 0)) 
+						(.close ~(get references 0))
+					)
+				)
+			)
+		) (println "Arguments (Vector, Expression)")
+	)
+)
