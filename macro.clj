223ae60e83f5878f140fcce3155b0971dde94729
frno0254 <smallgreycreatures@gmail.com>
2014-12-17 11:54:56 +0100

    Reason to use a macro -not unknown

--- macro.clj
+++ macro.clj
@@ -6,6 +6,7 @@
 		(let [~(get references 0) ~(get references 1)]
 			(try
 				~expression
+				(catch Exception e# e#)
 				(finally 
 					(if (instance? java.io.Closeable ~(get references 0)) 
 						(.close ~(get references 0))
@@ -15,3 +16,10 @@
 		) (println "Arguments (Vector, Expression)")
 	)
 )
+
+; Lite tankar om detta!
+; Detta macro skulle inte fungera att definera som en funktion eftersom argumenten som skickas in inte kan utvärderas direkt utan 
+; komplikationer. Detta är nämligen en av skillnaderna mellan ett macro och en funktion; att argumenten till en funktion utvärderas när
+; de skickas in till funktionen emedan de för ett macro ej gör det. I detta fall så skulle man ifall argumenten utvärderades direkt 
+; få problem med att exceptions uppstår som aldrig fångas. Om man skickar in en fil och får FileNotFoundException från File men också 
+; har öppnat en FileReader så skulle man med en funktion ej kunna stänga FileReadern, vilket man med detta macro alltid gör.
